987204e6fa61907daab4f318b495fec030986535
hrumichal <hru.michal@gmail.com>
2024-02-02 13:48:23 +0100

    Adding features for storage migration
    Bucket selection

--- app/migrate.py
+++ app/migrate.py
@@ -30,80 +30,94 @@ def main():
     source_api_token = st.sidebar.text_input("Source Project Storage API Token", type="password")
     destination_api_token = st.sidebar.text_input("Destination Project Storage API Token", type="password")
 
-    # MAIN CODE 
-    st.header("Migrate Configurations")
+    # Create tabs using radio buttons
+    tab_selection = st.selectbox("What do you want to migrate?", ["Migrate Configurations", "Migrate Storage"])
+
     if source_project_host and source_api_token and destination_project_host and destination_api_token:
-        # Migrate Configurations in the main area
-        st.markdown("All components will be migrated unless you select 'Keep' or 'Skip' to only migrate or skip selected Component IDs.")
-        processing_detail = st.selectbox("Processing Detail", ["", "Keep", "Skip"])
-        component_options = get_component_ids(source_project_host, {'X-StorageApi-Token':source_api_token})
-        component_ids = []
-        if processing_detail:
-            if processing_detail == "Keep":
-                st.markdown("Select Component IDs you want to keep (migrate)")
-            elif processing_detail == "Skip":
-                st.markdown("Select Component IDs you want to skip (not migrate)")
-            component_ids = st.multiselect("Component IDs", component_options)
+        if tab_selection == "Migrate Configurations":
+            # Migrate Configurations in the main area
+            st.header("Migrate Configurations")
+            st.markdown("All components will be migrated unless you select 'Keep' or 'Skip' to only migrate or skip selected Component IDs.")
+            processing_detail = st.selectbox("Processing Detail", ["", "Keep", "Skip"])
+            component_options = get_component_ids(source_project_host, {'X-StorageApi-Token':source_api_token})
+            component_ids = []
+            if processing_detail:
+                if processing_detail == "Keep":
+                    st.markdown("Select Component IDs you want to keep (migrate)")
+                elif processing_detail == "Skip":
+                    st.markdown("Select Component IDs you want to skip (not migrate)")
+                component_ids = st.multiselect("Component IDs", component_options)
 
-        skip = None
-        keep = None
-        if processing_detail == 'Skip':
-            skip = component_ids
-        elif processing_detail == 'Keep':
-            keep = component_ids
+            skip = None
+            keep = None
+            if processing_detail == 'Skip':
+                skip = component_ids
+            elif processing_detail == 'Keep':
+                keep = component_ids
 
-        only_selected_configs = st.checkbox("Migrate only particular configurations", value=False)
+            only_selected_configs = st.checkbox("Migrate only particular configurations", value=False)
 
-        if only_selected_configs:
-            configuration_options = None
-            if skip:
-                st.write('Fetching available configurations besides the components you wanted to skip...')
-                configuration_options = get_component_configurations(source_project_host, {'X-StorageApi-Token':source_api_token}, component_ids, 'skip')
-            elif keep:
-                st.write('Fetching available configurations for components you selected...')
-                configuration_options = get_component_configurations(source_project_host, {'X-StorageApi-Token':source_api_token}, component_ids, 'keep')
+            if only_selected_configs:
+                configuration_options = None
+                if skip:
+                    st.write('Fetching available configurations besides the components you wanted to skip...')
+                    configuration_options = get_component_configurations(source_project_host, {'X-StorageApi-Token':source_api_token}, component_ids, 'skip')
+                elif keep:
+                    st.write('Fetching available configurations for components you selected...')
+                    configuration_options = get_component_configurations(source_project_host, {'X-StorageApi-Token':source_api_token}, component_ids, 'keep')
+                else:
+                    st.write('Fetching available configurations for all components...')
+                    configuration_options = get_component_configurations(source_project_host, {'X-StorageApi-Token':source_api_token}, None, 'all')
+
+                if configuration_options:
+                    configuration_ids = st.multiselect("Configuration IDs", configuration_options)
+
+            if st.button("Migrate Configurations"):
+                # Execute migrate configurations script
+                HEAD = {'X-StorageApi-Token':source_api_token}
+                HEAD_FORM = {'X-StorageApi-Token':source_api_token, 'Content-Type': 'application/x-www-form-urlencoded'}
+                HEAD_DEST = {'X-StorageApi-Token':destination_api_token}
+                HEAD_FORM_DEST = {'X-StorageApi-Token':destination_api_token, 'Content-Type': 'application/x-www-form-urlencoded'}
+                BRANCH_DEST = 'default'
+
+                if only_selected_configs and len(configuration_ids)>0:
+                    configs = get_keboola_configs(source_project_host, HEAD, skip, keep, configuration_ids)
+                else:
+                    configs = get_keboola_configs(source_project_host, HEAD, skip, keep)
+                
+                fails = migrate_configs(source_project_host, HEAD, configs, HEAD_DEST, HEAD_FORM_DEST, BRANCH_DEST, DEBUG=False)
+                
+                st.write("Migration completed. Failures:", fails)
+                
+        elif tab_selection == "Migrate Storage":
+            # Migrate Storage Object Definitions in the main area
+            st.header("Migrate Storage Object Definitions")
+
+            # Fetch buckets and tables details
+            buckets_to_migrate, tables_to_migrate = export_storage_metadata(source_project_host, source_api_token)
+            
+            # Destination project has native data types feature
+            native_data_types = st.checkbox("Destination project should use Native Data Types", value=False)
+            
+            # Migrate only selected buckets or tables
+            only_selected_objects = st.checkbox("Migrate only selected buckets", value=False)
+            
+            if only_selected_objects:
+                bucket_ids_to_migrate_selection = st.multiselect("Bucket IDs", [buckets_to_migrate[i]['id'] for i in range(len(buckets_to_migrate))])
             else:
-                st.write('Fetching available configurations for all components...')
-                configuration_options = get_component_configurations(source_project_host, {'X-StorageApi-Token':source_api_token}, None, 'all')
+                bucket_ids_to_migrate_selection = None
 
-            if configuration_options:
-                configuration_ids = st.multiselect("Configuration IDs", configuration_options)
-
-        if st.button("Migrate Configurations"):
-            # Execute migrate configurations script
-            HEAD = {'X-StorageApi-Token':source_api_token}
-            HEAD_FORM = {'X-StorageApi-Token':source_api_token, 'Content-Type': 'application/x-www-form-urlencoded'}
-            HEAD_DEST = {'X-StorageApi-Token':destination_api_token}
-            HEAD_FORM_DEST = {'X-StorageApi-Token':destination_api_token, 'Content-Type': 'application/x-www-form-urlencoded'}
-            BRANCH_DEST = 'default'
-
-            
-
-            if only_selected_configs and len(configuration_ids)>0:
-                configs = get_keboola_configs(source_project_host, HEAD, skip, keep, configuration_ids)
-            else:
-                configs = get_keboola_configs(source_project_host, HEAD, skip, keep)
-            
-            fails = migrate_configs(source_project_host, HEAD, configs, HEAD_DEST, HEAD_FORM_DEST, BRANCH_DEST, DEBUG=False)
-            
-            st.write("Migration completed. Failures:", fails)
-
-        # Migrate Storage Object Definitions in the main area
-        st.header("Migrate Storage Object Definitions")
-        native_data_types = st.checkbox("Destination project should use Native Data Types", value=False)
-
-        if st.button("Migrate Storage Object Definitions"):
-            # Execute migrate storage object definitions script
-            if not native_data_types:
-                buckets_to_migrate, tables_to_migrate = export_storage_metadata(source_project_host, source_api_token)
-                migrate_buckets(destination_project_host, destination_api_token, buckets_to_migrate)
-                migrate_tables(destination_project_host, destination_api_token, tables_to_migrate)
-                st.write("Migration completed.")
-            else: 
-                buckets_to_migrate, tables_to_migrate = export_storage_metadata(source_project_host, source_api_token)
-                migrate_buckets(destination_project_host, destination_api_token, buckets_to_migrate)
-                migrate_tables_native_types(destination_project_host, destination_api_token, tables_to_migrate)
-                st.write("Migration completed.")
+            # Migrate
+            if st.button("Migrate Storage Object Definitions"):
+                # Execute migrate storage object definitions script
+                if not native_data_types:
+                    migrate_buckets(destination_project_host, destination_api_token, buckets_to_migrate, bucket_ids_to_migrate_selection)
+                    migrate_tables(destination_project_host, destination_api_token, tables_to_migrate, bucket_ids_to_migrate_selection)
+                    st.write("Migration completed.")
+                else: 
+                    migrate_buckets(destination_project_host, destination_api_token, buckets_to_migrate, bucket_ids_to_migrate_selection)
+                    migrate_tables_native_types(destination_project_host, destination_api_token, tables_to_migrate, bucket_ids_to_migrate_selection)
+                    st.write("Migration completed.")
     else:
         st.markdown("Enter hostnames and tokens for both source and destination projects to proceed...")
 
--- app/storage_migrator.py
+++ app/storage_migrator.py
@@ -45,13 +45,23 @@ def export_storage_metadata(BASE, TOKEN):
     #     json.dump(tables_to_migrate_metadata, f)
     return buckets_to_migrate, tables_to_migrate_metadata
 
-def migrate_buckets(BASE, TOKEN, BUCKETS):
+def migrate_buckets(BASE, TOKEN, BUCKETS, BUCKET_IDS_SELECTION = None):
     HEAD = {'X-StorageApi-Token': TOKEN}
 
     # Read buckets data from file
     buckets = BUCKETS
+    selected_buckets = []
 
-    for bucket in buckets:
+    if BUCKET_IDS_SELECTION:
+        for bucket in buckets:
+            if bucket['id'] in BUCKET_IDS_SELECTION:
+                selected_buckets.append(bucket)
+    else:
+        for bucket in buckets:
+            selected_buckets.append(bucket)
+
+    
+    for bucket in selected_buckets:        
         values = {
             "name": bucket['id'].split('.c-')[-1],
             "stage": bucket['stage'],
@@ -73,11 +83,22 @@ def migrate_buckets(BASE, TOKEN, BUCKETS):
             if new_bucket.status_code != 201:
                 st.write('Failed to create bucket:', new_bucket.status_code, new_bucket.text)
 
-def migrate_tables(BASE, TOKEN, TABLES):
+def migrate_tables(BASE, TOKEN, TABLES, BUCKET_IDS_SELECTION = None):
     HEAD = {'X-StorageApi-Token': TOKEN}
     fails = []
 
-    for table in TABLES:
+    selected_tables = []
+
+    if BUCKET_IDS_SELECTION:
+        for table in TABLES:
+            bucket_id = '.'.join(table['id'].split('.')[:2])
+            if bucket_id in BUCKET_IDS_SELECTION:
+                selected_tables.append(table)
+    else:
+        for table in TABLES:
+            selected_tables.append(table)
+
+    for table in selected_tables:
         bucket_id = '.'.join(table['id'].split('.')[:2])
         values = {
             "name": table['name'],
@@ -101,12 +122,23 @@ def migrate_tables(BASE, TOKEN, TABLES):
 
     return fails
 
-def migrate_tables_native_types(BASE, TOKEN, TABLES):
+def migrate_tables_native_types(BASE, TOKEN, TABLES, BUCKET_IDS_SELECTION = None):
     HEAD = {'X-StorageApi-Token': TOKEN}
     fails = []
 
+    selected_tables = []
+
+    if BUCKET_IDS_SELECTION:
+        for table in TABLES:
+            bucket_id = '.'.join(table['id'].split('.')[:2])
+            if bucket_id in BUCKET_IDS_SELECTION:
+                selected_tables.append(table)
+    else:
+        for table in TABLES:
+            selected_tables.append(table)                
+                
     # Create tables in destination project
-    for t in TABLES:
+    for t in selected_tables:
         bucket_id = '.'.join(t['id'].split('.')[:2])   
         
         values = {
@@ -118,41 +150,46 @@ def migrate_tables_native_types(BASE, TOKEN, TABLES):
             if 'columnMetadata' in t and len(t['columnMetadata'])>0:
                 st.write('Table', t['name'], 'will be created with defined data types...')
                 for c in t['columns']: 
-                    for cmd in t['columnMetadata']:
-                        if c == cmd:
-                            nullable = False
-                            length = False
-                            basetype = 'STRING'
-                            for metadata in t['columnMetadata'][c]:
-                                if metadata['key'] == 'KBC.datatype.basetype':
-                                    basetype = metadata['value']
-                                if metadata['key'] == 'KBC.datatype.length':
-                                    length = metadata['value']
-                                if metadata['key'] == 'KBC.datatype.nullable':
-                                    if metadata['value'] == '1':
-                                        nullable = True
+                    metadata = t['columnMetadata'].get(c, [])  # Get metadata for the current column, or an empty list if not found
+                    nullable = False
+                    length = False
+                    basetype = 'STRING'
+                    for m in metadata:
+                        if m['key'] == 'KBC.datatype.basetype':
+                            basetype = m['value']
+                        elif m['key'] == 'KBC.datatype.length':
+                            length = m['value']
+                        elif m['key'] == 'KBC.datatype.nullable':
+                            if m['value'] == '1':
+                                nullable = True
+                            else:
+                                nullable = False
+                    
+                    if c in t['primaryKey']:
+                        nullable = False
 
                     if length:
-                        new_column={"name":c,"definition":{"type":basetype,"nullable":nullable,"length":length}}
+                        new_column = {"name": c, "definition": {"type": basetype, "nullable": nullable, "length": length}}
                     else:
-                        new_column={"name":c,"definition":{"type":basetype,"nullable":nullable}}
-                            
+                        new_column = {"name": c, "definition": {"type": basetype, "nullable": nullable}}
+
                     values['columns'].append(new_column)
             else:
                 st.write('Table', t['name'], 'is not a Native Types table on the source - it will be created with Varchar MAX types...')
                 for c in t['columns']:
                     new_column={"name":c} 
                     values['columns'].append(new_column)
-
+            
             #Create table
             new_table = requests.post(url=''.join([BASE, 'v2/storage/buckets/', bucket_id, '/tables-definition']),
                                       headers = HEAD,
                                       json = values)
+
             if new_table.status_code != 202:
-                print('Table failed:', t['name'])
+                st.write('Table failed:', t['name'])
                 fails.append([t, new_table.text])
             else:
-                print('Table created:', t['name'])
+                st.write('Table created:', t['name'])
         except Exception as e:
-            print('Table failed:', t['name'], str(e))
+            st.write('Table failed:', t['name'], str(e))
             fails.append(t)                
